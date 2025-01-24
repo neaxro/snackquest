ff68be386f86d41097085ba7a7d0f765944dae27
Axel Roland Nemes <neaxro@gmail.com>
2025-01-24 19:35:33 +0100

    Added basic machine files as an example.

--- web/app.py
+++ web/app.py
@@ -26,5 +26,12 @@ def calculator():
 def home():
     return render_template("home.html")
 
+@app.route("/machines")
+def machines():
+    return render_template("machines.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        host="0.0.0.0"
+    )

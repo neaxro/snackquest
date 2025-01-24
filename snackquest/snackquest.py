4e22a3401d4ba9a2e601a11a41c2494a08f7111d
Axel Roland Nemes <neaxro@gmail.com>
2025-01-24 15:00:10 +0100

    Use PuLP library for solving problem.

--- snackquest/snackquest.py
+++ snackquest/snackquest.py
@@ -1,171 +1,25 @@
-import yaml, sys, argparse, logging, time, random
-import beeprint as bprint
-from models.snack import Snack
-from models.order import Order, Orders
-from models.graph import *
-from rich.console import Console
-from rich.table import Table
-from rich.padding import Padding
+import argparse
+import sys
 
-# Set the argument flags
-parser = argparse.ArgumentParser(
-    prog="Vending Machine Loot Calculator",
-    description="From the given balance and menu list calculates the optimal order to leave as less money on the card as possible."
-)
-parser.add_argument("balance", help="The current balance of the card [default JMF]", type=int)
-parser.add_argument("menu", help="Vending machine's menu [.yml, .yaml]", type=argparse.FileType("r"))
-parser.add_argument("-p", "--printMenu", dest="print_menu", action="store_true", help="Prints the menu")
-parser.add_argument("-i", "--info", dest="log_info", action="store_true", help="Prints log messages")
-parser.add_argument("-l", "--limit", dest="limit", type=int, default=3, help="Possible order solution limit")
-parser.add_argument("-o", "-out", dest="out_file", type=argparse.FileType("w", encoding="UTF-8"), help="The output file path")
-args = parser.parse_args()
+from solver import solve_problem
 
-# Set the logging config
-if args.log_info:
-    logging.basicConfig(level=logging.INFO)
-else:
-    logging.basicConfig(level=logging.WARNING)
 
-# Calculates the best order
-def _calc_order(balance: int, snacks: list[Snack]):
-    orders: list[Orders] = []
-    desired_items: list[Order] = []
+def _init_args():
+    # Set the argument flags
+    parser = argparse.ArgumentParser(
+        prog="Vending Machine Loot Calculator",
+        description="From the given balance and menu list calculates the optimal order to leave as less money on the card as possible."
+    )
+    parser.add_argument("balance", help="The current balance of the card [default JMF]", type=int)
+    parser.add_argument("menu", help="Vending machine's menu [.yml, .yaml]", type=argparse.FileType("r"))
+    parser.add_argument("-p", "--printMenu", dest="print_menu", action="store_true", help="Prints the menu")
+    parser.add_argument("-o", "-out", dest="out_file", type=argparse.FileType("w", encoding="UTF-8"), help="The output file path")
     
-    # Pick out the desired items first
-    logging.info("Picking out the desired items...")
-    for snack in snacks:
-        if snack.desired > 0:
-            balance -= snack.price * snack.desired
-            snack.tobuy = snack.desired
-            desired_items.append(
-                Order(
-                    snack.price,
-                    snack.desired,
-                    {snack.name}
-                )
-            )
+    return parser.parse_args()
 
-        snack.desired = 0
-    
-    if balance <= 0:
-        print("Your balance fall below 0 JMF, too much desired snacks!")
-        sys.exit(1)
-    
-    # Graph algorithm
-    distinct_prices = []
-    for snack in snacks:
-        distinct_prices.append(snack.price)
-    distinct_prices = set(distinct_prices)
-    
-    # Build a graph for one price (start_snack_price)
-    #   with this the alg is able to find the best possible solution
-    #   it makes the program much faster
-    start_snack_price = snacks[0].price
-    root = Node(start_snack_price, balance)
-    logging.info("Building graph...")
-    start_time = time.time()
-    build_graph(root, distinct_prices)
-    end_time = time.time()
-    logging.info(f"Graph building finished! ({round(end_time - start_time, 1)} seconds)")
-
-    # Find the optimal child in graph
-    logging.info("Finding the best order list...")
-    possible_orders: list[Node] = my_dfs(root)
-
-    # Find the minimal balance    
-    min_balance = possible_orders[0].balance
-    for po in possible_orders:
-        if po.balance < min_balance:
-            min_balance = po.balance
-    
-    minimals = list(set(filter(lambda o: o.balance == min_balance, possible_orders)))
-    
-    # Create possible orders
-    logging.info("Creating order list...")
-    limit = args.limit
-    for i in range(limit):
-        random_index = random.randint(0, len(minimals)-1)
-        
-        total_orders = desired_items.copy()
-        total_orders.extend(minimals[random_index].get_order(snacks))
-        
-        orders.append(
-            Orders(
-                min_balance,
-                total_orders
-            )
-        )
-        total_orders = []
-        
-    return orders
-
-# Loads the menu from the given yaml file
-def load_menu() -> list[Snack]:
-    menu = []
-    menu_yaml = None
-    
-    try:
-        menu_yaml = yaml.safe_load(args.menu)
-
-    except ValueError:
-        print("Invalid value!")
-        sys.exit(1)
-
-    except FileNotFoundError:
-        print("File was not found!")
-        sys.exit(1)
-        
-    if args.print_menu:
-        bprint.pp(menu_yaml)
-
-    menu_yaml = menu_yaml.get('items')
-    
-    for item in menu_yaml:
-        if(item['available']):
-            menu.append(
-                Snack(
-                    name = item['name'],
-                    price = item['price'],
-                    desired = item['desired'],
-                )
-            )
-        
-    return menu
-
-def _print_solutions(solutions: list[Orders]):
-    console = Console()
-    
-    for solution_index in range(len(solutions)):
-        table = Table(
-            title=f"Solution #{solution_index + 1}",
-            caption=f"Final balance: {solutions[solution_index].balance} JMF",
-            )
-        
-        # Add columns
-        table.add_column("Snack Options")
-        table.add_column("Count")
-        table.add_column("Price", justify="right")
-        
-        # Add rows
-        sum = 0
-        for order in solutions[solution_index].orders:
-            sum += order.price * order.count
-            table.add_row(
-                order.get_snacks(),
-                str(order.count),
-                str(order.price),
-                )
-        
-        # Add footer
-        table.add_section()
-        table.add_row("", "Total", str(sum))
-        
-        # Print table
-        console.print(Padding("", (1, 0, 0, 0)))
-        console.print(table)
-
-# MAIN
 def main():
+    args = _init_args()
+    
     if args.balance is None:
         print("Balance was not given!")
         sys.exit(1)
@@ -173,23 +27,12 @@ def main():
     if args.menu is None:
         print("Menu list path was not given!")
         sys.exit(1)
-    
-    logging.info("Loading menu...")
-    menu = load_menu()
-    logging.info("Menu loaded!")
-    
-    possible_orders = _calc_order(args.balance, menu)
-    logging.info("Searching finished!")
-    
+        
     if args.out_file is not None:
         sys.stdout = args.out_file
         
-    """ print("Possible orders:")
-    for po in possible_orders:
-        po.show() """
-    
-    _print_solutions(possible_orders)
+    # Solve problem.
+    solve_problem(args.balance, args.menu, args.print_menu)
 
 if __name__ == "__main__":
     main()
-    
---
+++ snackquest/solver.py
@@ -0,0 +1,102 @@
+import yaml
+import pulp
+import beeprint as bprint
+
+from rich.console import Console
+from rich.table import Table
+from rich.padding import Padding
+
+
+def solve_problem(balance, menu_file, print_menu=False):
+    data = yaml.safe_load(menu_file)
+    budget = balance
+    
+    if print_menu:
+        bprint.pp(data)
+
+    problem = pulp.LpProblem("Maximize_Candies", pulp.LpMaximize)
+
+    # Init variables
+    variables = {}
+    for item in data['items']:
+        if item['available']:  # Use only the available candies.
+            variables[item['name']] = pulp.LpVariable(
+                item['name'], lowBound=0, cat='Integer'
+            )
+
+    # Target function: Maxminal candy count.
+    problem += pulp.lpSum(variables[item['name']] for item in data['items'] if item['available'])
+
+    # Limit: Money
+    problem += pulp.lpSum(
+        variables[item['name']] * item['price']
+        for item in data['items']
+        if item['available']
+    ) <= budget
+
+    # Limit: Min count of specific candies.
+    for item in data['items']:
+        if item['available']:
+            problem += variables[item['name']] >= item['desired']
+
+    # Solve.
+    problem.solve()
+
+    # Show results.
+    if problem.status == pulp.LpStatusOptimal:
+        final_items = []
+
+        total_candies = 0
+        total_cost = 0
+
+        for item in data['items']:
+            if item['available']:
+                count = int(variables[item['name']].varValue)
+                total_candies += count
+                total_cost += count * item['price']
+                
+                # Collect final items.                
+                final_items.append(
+                    {
+                        'name': item['name'],
+                        'count': count,
+                        'unit_price': item['price']
+                    }
+                )
+        
+        _print_solution(balance, final_items, total_candies, total_cost)
+    else:
+        print("No solution found.")
+
+def _print_solution(balance, final_items, total_candies, total_cost):
+    console = Console()
+    
+    # Create table
+    table = Table(
+        title=f"The Optimal Solution",
+        caption=f"Initial budget: {balance} JMF",
+    )
+    
+    # Add columns
+    table.add_column("Snack Options")
+    table.add_column("Count")
+    table.add_column("Unit price (JMF)", justify="right")
+    
+    # Add rows
+    for item in final_items:
+        table.add_row(
+            item['name'],
+            str(item['count']),
+            str(item['unit_price'])
+        )
+
+    # Add footer
+    table.add_section()
+    table.add_row("Total", str(total_candies), str(total_cost))
+    table.add_row("Remaining", "", str(balance-total_cost))
+    table.add_section()
+    table.add_row("Initial budget", "", str(balance))
+
+    # Print table
+    console.print(Padding("", (0, 0, 0, 0)))
+    console.print(table)
